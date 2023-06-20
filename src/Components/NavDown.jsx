f2bace7631c132d78b681fa22fdeea5da0615055
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2023-06-20 21:18:37 +0100

    add the side navigation bar and some simple styling

---
+++ src/Components/NavDown.jsx
@@ -0,0 +1,14 @@
+const NavDown = () => {
+    return (
+        <nav class="side-nav">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/links">Links</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavDown
